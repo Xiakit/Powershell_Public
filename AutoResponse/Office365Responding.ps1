4ef8ef5f483dd21e35a147044f9129c4e6654e31
Pascal Grossrieder <pascal.grossrieder@yr-group.ch>
2018-07-11 18:01:52 +0200

    Adding debugging and configfile

--- AutoResponse/Office365Responding.ps1
+++ AutoResponse/Office365Responding.ps1
@@ -7,23 +7,50 @@ Credits to https://sysadminben.wordpress.com/2015/10/27/reading-emails-from-offi
 Credits to http://www.garrettpatterson.com/2014/04/18/checkread-messages-exchangeoffice365-inbox-with-powershell/
 Donwload DLL from here https://www.microsoft.com/en-us/download/details.aspx?id=42951
 #>
+$Workfolder = "C:\Users\grossriederp\Documents\Powershell_Public\AutoResponse"
 
-Start-Transcript -Path "$PSScriptRoot\Transcript.log"
+#Creating config
+if(!(Test-Path "$Workfolder\Settings.txt")){
+    $ExampleConfig = "[General]
+MyMail=pascal.grossrieder@learning.ifa.ch
+DayAbsent=Friday
+Domain=ifa.ch
+CheckEverXMinutes=5
+[SendingMail]
+From=blabla.ch
+SmtpServer=smpt.blabla.ch
+Subject=Out of Office.
+MyOutOfOfficeMessage=Jeweils Freitags nicht im Office.
+[Debugging]
+Debugging=False"
+    $ExampleConfig | Out-File -FilePath "$Workfolder\Settings.txt"
+    Write-Host "Please check the config file ("$Workfolder\Settings.txt") `r`n and add the needed details, you will be asked for your password when you run this script again." -ForegroundColor Yellow
+    Read-Host -Prompt "Press Enter to continue."
+}
+
 #Loading the config
-Get-Content "$PSScriptRoot\Settings.txt" | foreach-object -begin { $config=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $config.Add($k[0], $k[1]) } }
-$Workfolder = $PSScriptRoot
+Get-Content "$Workfolder\Settings.txt" | foreach-object -begin { $config=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $config.Add($k[0], $k[1]) } }
+
 #Configuration
 #Configuration to log in
 $MyMail = $config.MyMail         #Adress used in Office365
 $Domain = $config.Domain               #Domain name used for the authentication process in office 365 "mycompany.com" for example
 $DayAbsent = $config.DayAbsent         #On what days should the script run
 $CheckEverXMinutes = [int]$config.CheckEverXMinutes     #Interval to check for new messages
+$Debugging = [bool]$config.Debugging
+
+
+if($Debugging){
+    Start-Transcript -Path "$Workfolder\Transcript.log"
+    $DebugPreference = Continue
+}
+
 
 #To save your password secure in a file
-if(!(Test-Path $PSScriptRoot\cred.txt)){
+if(!(Test-Path $Workfolder\cred.txt)){
     Read-Host -AsSecureString -Prompt "Type your Password" | ConvertFrom-SecureString | Out-File -FilePath "$Workfolder\cred.txt" -Force
 }
-$Password = Get-Content -Path "$PSScriptRoot\cred.txt" | ConvertTo-SecureString
+$Password = Get-Content -Path "$Workfolder\cred.txt" | ConvertTo-SecureString
 
 #Configuration to send messages
 $From = $config.From #The adress the mail will be sent from, if possible use the same domain in the adress as the smtpserver in order to avoid the junk folder :)
@@ -67,11 +94,11 @@ if (!(Test-Path -Path "C:\Program Files\Microsoft\Exchange\Web Services\2.2\Micr
     throw "The Required Web Services Managed API 2.2 is not available, please download and install it. Download it at https://www.microsoft.com/en-us/download/details.aspx?id=42951"
 }
 
-if ((get-date).DayOfWeek -notlike "*$DayAbsent*") {
+if ((get-date).DayOfWeek -notlike "$DayAbsent") {
     Write-Host "It is not $DayAbsent, you need to define $((get-date).DayOfWeek) in the script in order to auto respond today."
 }
 else {
-    $SentMailsLog = "$Psscriptroot\Sentmails.log"
+    $SentMailsLog = "$Workfolder\Sentmails.log"
     Remove-Item -Path $SentMailsLog -Force -ErrorAction SilentlyContinue
 
     while ((get-date).DayOfWeek -like "$DayAbsent") {
@@ -91,4 +118,8 @@ else {
         Start-Sleep -Seconds ($CheckEverXMinutes*60)
     }
 }
-Stop-Transcript
+
+if($Debugging){
+    Stop-Transcript
+}
+
